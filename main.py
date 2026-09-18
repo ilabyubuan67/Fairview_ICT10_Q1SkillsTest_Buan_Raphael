4b39a7334fbe8587cc29ebc18bffb4f93e2f91e5
ilabyubuan67 <buan.rg@obmontessori.edu.ph>
2026-09-18 18:11:28 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from pyscript import display, document
+
+def calculate_total(e):
+    document.getElementById("receipt").innerHTML = ""
+
+    shoyu_ramen = document.getElementById("shoyu_ramen").checked
+    miso_ramen = document.getElementById("miso_ramen").checked
+    tonkotsu_ramen = document.getElementById("tonkotsu_ramen").checked
+    spicy_ramen = document.getElementById("spicy_ramen").checked
+
+    subtotal = (shoyu_ramen * 180) + (miso_ramen * 200) + (tonkotsu_ramen * 220) + (spicy_ramen * 190)
+
+    tax = subtotal * 0.12
+    total = subtotal + tax
+
+    display("Order Receipt", target="receipt")
+    display(f"Subtotal: ₱{subtotal:f}", target="receipt")
+    display(f"VAT (12%): ₱{tax:f}", target="receipt")
+    display(f"Total: ₱{total:f}", target="receipt")
